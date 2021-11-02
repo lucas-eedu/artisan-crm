6ec34770a06c46116c30a21b9dc140e79a07772a
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-02 17:31:13 -0300

    MCriação da Migration e relacionamentos nos models

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile() {
+        return $this->belongsTo(Profile::class);
+    }
 }
