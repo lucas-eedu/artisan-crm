69d6a7c6622466ac0b77270cd6e164a8081af999
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-08 23:19:14 -0300

    PermissionController

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -16,6 +16,7 @@ class PermissionController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->authorizeResource(Permission::class, 'permission');
     }
     
     /**
