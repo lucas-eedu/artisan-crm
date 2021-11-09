3b32c16b9a086da457b8f42e88ebacb30b52a5c5
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-08 22:56:04 -0300

    Request UserPolicy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,7 @@ class UserController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->authorizeResource(User::class, 'user');
     }
 
     /**
