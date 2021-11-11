7c2bfdf200bd2f0226b3305716b38ebe90acdba4
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-10 22:28:13 -0300

    Preparando view para editar perfil e ajuste no rodapé

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -125,15 +125,20 @@
             <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
          </div>
       </li>
-      <li class="nav-item">
-         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-         <i class="fas fa-expand-arrows-alt"></i>
+      <!-- Profile Dropdown Menu -->
+      <li class="nav-item dropdown">
+         <a class="nav-link" data-toggle="dropdown" href="#" style="padding-top:4px;">
+            <img src="{{ asset('template/dist/img/user1-128x128.jpg') }}" alt="{{auth()->user()->name}}" class="img-circle" style="width:30px;height:auto;">
          </a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-         <i class="fas fa-th-large"></i>
-         </a>
+         <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
+            <a href="#" class="dropdown-item">
+               <i class="fas fa-user mr-2"></i> Meu Perfil
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" title="Sair do sistema">
+               <i class="fas fa-power-off mr-2"></i> Sair
+            </a>
+         </div>
       </li>
    </ul>
 </nav>
@@ -148,31 +153,9 @@
    </a>
    <!-- Sidebar -->
    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-         <div class="image">
-            <img src="{{ asset('template/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-         </div>
-         <div class="info">
-            <a href="#" class="d-block">{{auth()->user()->name}}</a>
-         </div>
-      </div> --}}
-      <!-- SidebarSearch Form -->
-      {{-- <div class="form-inline">
-         <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-               <button class="btn btn-sidebar">
-               <i class="fas fa-search fa-fw"></i>
-               </button>
-            </div>
-         </div>
-      </div> --}}
       <!-- Sidebar Menu -->
       <nav class="mt-2">
          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
             <li class="nav-item">
                <a href="{{route('dashboard')}}" class="nav-link">
                   <i class="nav-icon fas fa-chart-pie"></i>
