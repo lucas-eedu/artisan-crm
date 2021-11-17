e2376dc6fb7d69dacc912a296c0b1ea073bdd1ce
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-17 20:16:59 -0300

    Criando tabela companies

---
+++ database/migrations/2021_11_17_213917_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->uuid('id')->primary()->nullable(false);
+            $table->index('id');
+            $table->string('name', 255);
+            $table->string('segment', 50)->nullable();
+            $table->string('state', 2);
+            $table->string('number_employees', 50)->nullable();
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
