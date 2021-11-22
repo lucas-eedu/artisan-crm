1aaf1cd8557bf08555f002df37af083374c4f87f
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-22 15:02:35 -0300

    Corrindo erro no code das policies

--- app/Policies/PermissionPolicy.php
+++ app/Policies/PermissionPolicy.php
@@ -18,7 +18,7 @@ class PermissionPolicy
      */
     public function viewAny(User $user)
     {
-        return $user->containsPermission('user_viewAny');
+        return $user->containsPermission('permission_viewAny');
     }
 
     /**
@@ -30,7 +30,7 @@ class PermissionPolicy
      */
     public function view(User $user, Permission $permission)
     {
-        return $user->containsPermission('user_viewAny');
+        return $user->containsPermission('permission_viewAny');
     }
 
     /**
@@ -41,7 +41,7 @@ class PermissionPolicy
      */
     public function create(User $user)
     {
-        return $user->containsPermission('user_create');
+        return $user->containsPermission('permission_create');
     }
 
     /**
@@ -53,7 +53,7 @@ class PermissionPolicy
      */
     public function update(User $user, Permission $permission)
     {
-        return $user->containsPermission('user_update');
+        return $user->containsPermission('permission_update');
     }
 
     /**
@@ -65,6 +65,6 @@ class PermissionPolicy
      */
     public function delete(User $user, Permission $permission)
     {
-        return $user->containsPermission('user_delete');
+        return $user->containsPermission('permission_delete');
     }
 }
