a09cd3adc97b8cf5b2bf2b15a7be8284d745c31f
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-22 17:53:12 -0300

    Ajustando o retorno ao errar nos campos com select2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,19 +33,19 @@
                      <div class="col-md-6">
                         <select id="segment" class="select2 form-control @error('segment') is-invalid @enderror" name="segment">
                            <option value="">Segmento</option>
-                           <option value="Tech ou Software">Tech ou Software</option>
-                           <option value="Imobiliário">Imobiliário</option>
-                           <option value="Educação e Ensino">Educação e Ensino</option>
-                           <option value="Agência Criativa (web, publicidade, vídeo)">Agência Criativa (web, publicidade, vídeo)</option>
-                           <option value="Serviços financeiros ou de crédito">Serviços financeiros ou de crédito</option>
-                           <option value="Notícias, imprensa e publicações">Notícias, imprensa e publicações</option>
-                           <option value="Fábrica ou Industria">Fábrica ou Industria</option>
-                           <option value="Consultoria e Coaching">Consultoria e Coaching</option>
-                           <option value="Comércio (varejo, atacado)">Comércio (varejo, atacado)</option>
-                           <option value="Automotivo">Automotivo</option>
-                           <option value="Saúde">Saúde</option>
-                           <option value="Construção">Construção</option>
-                           <option value="Outro">Outro</option>
+                           <option value="Tech ou Software" @if ("Tech ou Software" == old('segment')) selected="selected" @endif>Tech ou Software</option>
+                           <option value="Imobiliário" @if ("Imobiliário" == old('segment')) selected="selected" @endif>Imobiliário</option>
+                           <option value="Educação e Ensino" @if ("Educação e Ensino" == old('segment')) selected="selected" @endif>Educação e Ensino</option>
+                           <option value="Agência Criativa (web, publicidade, vídeo)" @if ("Agência Criativa (web, publicidade, vídeo)" == old('segment')) selected="selected" @endif>Agência Criativa (web, publicidade, vídeo)</option>
+                           <option value="Serviços financeiros ou de crédito" @if ("Serviços financeiros ou de crédito" == old('segment')) selected="selected" @endif>Serviços financeiros ou de crédito</option>
+                           <option value="Notícias, imprensa e publicações" @if ("Notícias, imprensa e publicações" == old('segment')) selected="selected" @endif>Notícias, imprensa e publicações</option>
+                           <option value="Fábrica ou Industria" @if ("Fábrica ou Industria" == old('segment')) selected="selected" @endif>Fábrica ou Industria</option>
+                           <option value="Consultoria e Coaching" @if ("Consultoria e Coaching" == old('segment')) selected="selected" @endif>Consultoria e Coaching</option>
+                           <option value="Comércio (varejo, atacado)" @if ("Comércio (varejo, atacado)" == old('segment')) selected="selected" @endif>Comércio (varejo, atacado)</option>
+                           <option value="Automotivo" @if ("Automotivo" == old('segment')) selected="selected" @endif>Automotivo</option>
+                           <option value="Saúde" @if ("Saúde" == old('segment')) selected="selected" @endif>Saúde</option>
+                           <option value="Construção" @if ("Construção" == old('segment')) selected="selected" @endif>Construção</option>
+                           <option value="Outro" @if ("Outro" == old('segment')) selected="selected" @endif>Outro</option>
                         </select>
                         @error('segment')
                            <span class="invalid-feedback" role="alert">
@@ -56,33 +56,33 @@
                      <div class="col-md-6">
                         <select id="state" class="select2 form-control @error('state') is-invalid @enderror" name="state">
                            <option value="">Estado</option>
-                           <option value="AC">Acre</option>
-                           <option value="AL">Alagoas</option>
-                           <option value="AP">Amapá</option>
-                           <option value="AM">Amazonas</option>
-                           <option value="BA">Bahia</option>
-                           <option value="CE">Ceará</option>
-                           <option value="DF">Distrito Federal</option>
-                           <option value="ES">Espirito Santo</option>
-                           <option value="GO">Goiás</option>
-                           <option value="MA">Maranhão</option>
-                           <option value="MS">Mato Grosso do Sul</option>
-                           <option value="MT">Mato Grosso</option>
-                           <option value="MG">Minas Gerais</option>
-                           <option value="PA">Pará</option>
-                           <option value="PB">Paraíba</option>
-                           <option value="PR">Paraná</option>
-                           <option value="PE">Pernambuco</option>
-                           <option value="PI">Piauí</option>
-                           <option value="RJ">Rio de Janeiro</option>
-                           <option value="RN">Rio Grande do Norte</option>
-                           <option value="RS">Rio Grande do Sul</option>
-                           <option value="RO">Rondônia</option>
-                           <option value="RR">Roraima</option>
-                           <option value="SC">Santa Catarina</option>
-                           <option value="SP">São Paulo</option>
-                           <option value="SE">Sergipe</option>
-                           <option value="TO">Tocantins</option>
+                           <option value="AC" @if ("AC" == old('state')) selected="selected" @endif>Acre</option>
+                           <option value="AL" @if ("AL" == old('state')) selected="selected" @endif>Alagoas</option>
+                           <option value="AP" @if ("AP" == old('state')) selected="selected" @endif>Amapá</option>
+                           <option value="AM" @if ("AM" == old('state')) selected="selected" @endif>Amazonas</option>
+                           <option value="BA" @if ("BA" == old('state')) selected="selected" @endif>Bahia</option>
+                           <option value="CE" @if ("CE" == old('state')) selected="selected" @endif>Ceará</option>
+                           <option value="DF" @if ("DF" == old('state')) selected="selected" @endif>Distrito Federal</option>
+                           <option value="ES" @if ("ES" == old('state')) selected="selected" @endif>Espirito Santo</option>
+                           <option value="GO" @if ("GO" == old('state')) selected="selected" @endif>Goiás</option>
+                           <option value="MA" @if ("MA" == old('state')) selected="selected" @endif>Maranhão</option>
+                           <option value="MS" @if ("MS" == old('state')) selected="selected" @endif>Mato Grosso do Sul</option>
+                           <option value="MT" @if ("MT" == old('state')) selected="selected" @endif>Mato Grosso</option>
+                           <option value="MG" @if ("MG" == old('state')) selected="selected" @endif>Minas Gerais</option>
+                           <option value="PA" @if ("PA" == old('state')) selected="selected" @endif>Pará</option>
+                           <option value="PB" @if ("PB" == old('state')) selected="selected" @endif>Paraíba</option>
+                           <option value="PR" @if ("PR" == old('state')) selected="selected" @endif>Paraná</option>
+                           <option value="PE" @if ("PE" == old('state')) selected="selected" @endif>Pernambuco</option>
+                           <option value="PI" @if ("PI" == old('state')) selected="selected" @endif>Piauí</option>
+                           <option value="RJ" @if ("RJ" == old('state')) selected="selected" @endif>Rio de Janeiro</option>
+                           <option value="RN" @if ("RN" == old('state')) selected="selected" @endif>Rio Grande do Norte</option>
+                           <option value="RS" @if ("RS" == old('state')) selected="selected" @endif>Rio Grande do Sul</option>
+                           <option value="RO" @if ("RO" == old('state')) selected="selected" @endif>Rondônia</option>
+                           <option value="RR" @if ("RR" == old('state')) selected="selected" @endif>Roraima</option>
+                           <option value="SC" @if ("SC" == old('state')) selected="selected" @endif>Santa Catarina</option>
+                           <option value="SP" @if ("SP" == old('state')) selected="selected" @endif>São Paulo</option>
+                           <option value="SE" @if ("SE" == old('state')) selected="selected" @endif>Sergipe</option>
+                           <option value="TO" @if ("TO" == old('state')) selected="selected" @endif>Tocantins</option>
                         </select>
                         @error('state')
                            <span class="invalid-feedback" role="alert">
@@ -95,11 +95,11 @@
                <div class="form-group mb-3">
                   <select id="number_employees" class="select2 form-control @error('number_employees') is-invalid @enderror" name="number_employees">
                      <option value="">Qual o tamanho da empresa?</option>
-                     <option value="1">1 Funcionário</option>
-                     <option value="2-10">2-10 Funcionários</option>
-                     <option value="11-50">11-50 Funcionários</option>
-                     <option value="51-100">51-100 Funcionários</option>
-                     <option value="101+">101+ Funcionários</option>
+                     <option value="1" @if ("1" == old('number_employees')) selected="selected" @endif>1 Funcionário</option>
+                     <option value="2-10" @if ("2-10" == old('number_employees')) selected="selected" @endif>2-10 Funcionários</option>
+                     <option value="11-50" @if ("11-50" == old('number_employees')) selected="selected" @endif>11-50 Funcionários</option>
+                     <option value="51-100" @if ("51-100" == old('number_employees')) selected="selected" @endif>51-100 Funcionários</option>
+                     <option value="101+" @if ("101+" == old('number_employees')) selected="selected" @endif>101+ Funcionários</option>
                   </select>
                   @error('number_employees')
                      <span class="invalid-feedback" role="alert">
