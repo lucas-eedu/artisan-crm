6727b9b8f5eacb2b560996a7e2004bd79253feed
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-22 17:41:05 -0300

    Criando tabela products com o relacionamento de company_id

---
+++ database/migrations/2021_11_22_182540_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary()->nullable(false);
+            $table->index('id');
+            $table->string('name', 255);
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+            $table->uuid('company_id');
+            $table->index('company_id');
+            $table
+                ->foreign('company_id', 'products_company')
+                ->references('id')
+                ->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_company');
+            $table->dropColumn('company_id');
+            $table->dropIfExists('products');
+        });
+    }
+}
