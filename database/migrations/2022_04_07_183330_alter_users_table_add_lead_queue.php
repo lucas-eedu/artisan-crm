ddb39e77b66d9bd71d92cb9f170b7d5d4b17fd5b
Lucas Eduardo <lucas01.dev@gmail.com>
2022-04-08 13:22:04 -0300

    feat(Lead): Automate the distribution of leads to admin users and sellers

--- database/migrations/2022_04_07_183330_alter_users_table_add_lead_queue.php
+++ database/migrations/2022_04_07_183330_alter_users_table_add_lead_queue.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterUsersTableAddProfilePicture extends Migration
+class AlterUsersTableAddLeadQueue extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AlterUsersTableAddProfilePicture extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_picture', 255)->nullable()->after('status');
+            $table->boolean('lead_queue')->default(false)->before('status');
         });
     }
 
@@ -26,7 +26,7 @@ class AlterUsersTableAddProfilePicture extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('photo');
+            $table->dropColumn('lead_queue');
         });
     }
 }
