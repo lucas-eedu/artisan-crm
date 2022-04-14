168f42ee19e6e64e7ef9c294aa515f9347bfa646
Lucas Eduardo <lucas01.dev@gmail.com>
2022-04-14 14:58:29 -0300

    [FEAT](Layout): Adjusting the dashboard screen layout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+   <strong>Powered by: <a href="https://github.com/lucas-eedu" target="_blank">Lucas Eduardo</a>.</strong>
+   <div class="float-right d-none d-sm-inline-block">
+         <b>Versão</b> v0.0.1
+   </div>
+</footer>
