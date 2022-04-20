d7a3d0cf6ddf73cc69eb269e03c1d7f530918c8f
Lucas Eduardo <lucas01.dev@gmail.com>
2022-04-20 17:52:08 -0300

    [FEAT](AutenticatePages): Creating new login page

---
+++ resources/views/auth/_login.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.guest')
+
+@section('content')
+   <div class="login-box">
+
+      @include('flash::message')
+
+      @foreach($errors->all() as $error)
+         <p class="alert alert-danger">{{$error}}</p>
+      @endforeach
+      
+      <!-- /.login-logo -->
+      <div class="card card-outline card-primary">
+         <div class="card-header text-center">
+            <a href="#" class="h1"><b>Artisan</b>CRM</a>
+         </div>
+         <div class="card-body">
+            <p class="login-box-msg">Faça login para iniciar sua sessão</p>
+            
+            <form action="{{ route('login') }}" method="post">
+               
+               @csrf
+
+               <div class="input-group mb-3">
+                  <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" id="email" placeholder="E-mail" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                  <div class="input-group-append">
+                     <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                     </div>
+                  </div>
+               </div>
+
+               <div class="input-group mb-3">
+                  <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password" id="user-password" placeholder="Informe a senha">
+                  <div class="input-group-append">
+                     <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                     </div>
+                  </div>
+               </div>
+               
+               <div class="row">
+                  <div class="col-8">
+                     <div class="icheck-primary">
+                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label for="remember">Lembre-se de mim</label>
+                     </div>
+                  </div>
+                  <div class="col-4">
+                     <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+                  </div>
+               </div>
+
+            </form>
+
+            {{-- <div class="social-auth-links text-center mt-2 mb-3">
+               <a href="#" class="btn btn-block btn-primary"><i class="fab fa-facebook mr-2"></i> Entre usando o Facebook</a>
+               <a href="#" class="btn btn-block btn-danger"><i class="fab fa-google-plus mr-2"></i> Entre usando o Google+&nbsp;&nbsp;&nbsp;</a>
+            </div> --}}
+
+            <!-- /.social-auth-links -->
+            @if (Route::has('password.request'))
+               <p class="mb-1"><a href="{{ route('password.request') }}">Esqueci minha senha</a></p>
+            @endif
+            <p class="mb-0"><a href="{{route('register')}}" class="text-center">Criar conta</a></p>
+
+         </div>
+         <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+   </div>
+@endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,74 @@
-@extends('layouts.guest')
+@extends('layouts.auth')
 
 @section('content')
-   <div class="login-box">
+<div class="min-h-screen bg-gray-100 md:flex flex-row justify-center">
+   <div class="hidden md:block sm:mx-auto sm:w-full bg-login-image bg-no-repeat bg-cover"></div>
+   <div class="px-5 sm:my-auto sm:mx-auto sm:w-full md:px-3">
 
       @include('flash::message')
 
       @foreach($errors->all() as $error)
-            <p class="alert alert-danger">{{$error}}</p>
+         <p class="alert alert-danger">{{$error}}</p>
       @endforeach
-      
-      <!-- /.login-logo -->
-      <div class="card card-outline card-primary">
-         <div class="card-header text-center">
-            <a href="#" class="h1"><b>Artisan</b>CRM</a>
-         </div>
-         <div class="card-body">
-            <p class="login-box-msg">Faça login para iniciar sua sessão</p>
-            
-            <form action="{{ route('login') }}" method="post">
-               
-               @csrf
 
-               <div class="input-group mb-3">
-                  <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" id="email" placeholder="E-mail" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
-                  <div class="input-group-append">
-                     <div class="input-group-text">
-                        <span class="fas fa-envelope"></span>
-                     </div>
-                  </div>
-               </div>
-
-               <div class="input-group mb-3">
-                  <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="current-password" id="user-password" placeholder="Informe a senha">
-                  <div class="input-group-append">
-                     <div class="input-group-text">
-                        <span class="fas fa-lock"></span>
-                     </div>
-                  </div>
-               </div>
-               
-               <div class="row">
-                  <div class="col-8">
-                     <div class="icheck-primary">
-                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <label for="remember">Lembre-se de mim</label>
-                     </div>
-                  </div>
-                  <div class="col-4">
-                     <button type="submit" class="btn btn-primary btn-block">Entrar</button>
-                  </div>
-               </div>
-
-            </form>
-
-            {{-- <div class="social-auth-links text-center mt-2 mb-3">
-               <a href="#" class="btn btn-block btn-primary"><i class="fab fa-facebook mr-2"></i> Entre usando o Facebook</a>
-               <a href="#" class="btn btn-block btn-danger"><i class="fab fa-google-plus mr-2"></i> Entre usando o Google+&nbsp;&nbsp;&nbsp;</a>
-            </div> --}}
-
-            <!-- /.social-auth-links -->
-            @if (Route::has('password.request'))
-               <p class="mb-1"><a href="{{ route('password.request') }}">Esqueci minha senha</a></p>
-            @endif
-            <p class="mb-0"><a href="{{route('register')}}" class="text-center">Criar conta</a></p>
-
-         </div>
-         <!-- /.card-body -->
+      <div class="sm:mx-auto sm:w-full sm:max-w-md mb-5">
+         <h2 class="pt-10 md:pt-0 text-center text-3xl font-extrabold text-gray-900">Artisan<span class="font-normal">CRM</span></h2>
+         <p class="mt-2 text-center text-md text-gray-600 max-w">Faça login para iniciar sua sessão:</p>
       </div>
-      <!-- /.card -->
+
+      <div class="bg-white py-8 px-5 sm:px-10 sm:mx-5 shadow rounded-lg">
+         <form class="mb-0 space-y-5" action="{{ route('login') }}" method="post">
+
+            @csrf
+
+            <div>
+               <label for="email" class="block font-medium text-gray-700">Email:</label>
+               <div class="mt-1">
+                  <input id="email" name="email" type="email" autocomplete="email" placeholder="Email" required class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:border-sky-500" />
+               </div>
+            </div>
+            
+            <div>
+               <div class="flex justify-between items-center">
+                  <label for="password" class="block font-medium text-gray-700">Senha</label>
+                  @if (Route::has('password.request'))
+                  <a href="{{ route('password.request') }}" class="font-medium text-sm text-sky-600 hover:text-sky-500">Esqueceu sua
+                     senha?</a>
+                  @endif
+               </div>
+               <div class="mt-1">
+                  <input id="password" name="password" type="password" placeholder="********" autocomplete="current-password" required class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:border-sky-500" />
+               </div>
+            </div>
+
+
+            <div class="flex items-center">
+               <input name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} type="checkbox" class="" />
+               <label for="remember" class="ml-2 block text-sm text-gray-900">Lembre-se de mim
+               </label>
+            </div>
+
+            <div>
+               <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sky-600 hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500">Entrar</button>
+            </div>
+         </form>
+         <p class="mt-5 text-center text-sm text-gray-600 max-w">
+            Ainda não tem uma conta?
+            <a href="{{route('register')}}" class="font-medium text-sky-600 hover:text-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500">Criar
+               conta</a>
+         </p>
+      </div>
    </div>
+</div>
+
+<!-- Personalizações -->
+<script>
+   tailwind.config = {
+      theme: {
+         backgroundImage: {
+            'login-image': "url({{ asset('artisancrmv1/assets/images/auth/bg-auth.jpg') }})",
+         }
+      }
+   }
+</script>
 @endsection
