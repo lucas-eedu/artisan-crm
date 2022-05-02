2d51d54fecf6ce852660ab302e81a78ad6c3b811
Lucas Eduardo <lucas01.dev@gmail.com>
2022-05-02 17:02:24 -0300

    [FEAT](RegisterPages): Improvements to the registration screen and the general construction of the blade structure

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,12 +5,20 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="csrf-token" content="{{ csrf_token() }}">
       <title>{{ config('app.name', 'Laravel') }}</title>
+      <!-- CDN Tailwind CSS -->
       <script src="https://cdn.tailwindcss.com"></script>
+      <!-- Select2 -->
+      <link rel="stylesheet" href="{{ asset('artisancrmv1/assets/plugins/select2/css/select2.min.css') }}">
+      <!-- Main CSS -->
+      <link rel="stylesheet" href="{{ asset('artisancrmv1/assets/css/main.css') }}">
    </head>
    <body>
 
       {{-- Content --}}
       @yield('content')
+
+      {{-- Scripts --}}
+      @yield('scripts')
       
    </body>
 </html>
